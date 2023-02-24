d22c8628eb1d4de1847a034a647afaa4d48ad6ab
Olga Bondarenko <bonnhelga86@yandex.ru>
2023-02-24 13:41:22 +0400

    feat: Открытие и закрытие popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,18 @@
-const profileEditOpen = document.querySelector('.profile__edit');
-const popupEditSave = document.querySelector('.popup__button');
-const popupEditClose = document.querySelector('.popup__close');
 const popup = document.querySelector('.popup');
+// Элементы взаимодействия с Popup
+const popupOpen = document.querySelectorAll('.popup-open');
+const popupEditSave = document.querySelector('.popup__button');
+const popupClose = document.querySelectorAll('.popup__close');
+// Элементы блока Profile
 const profileName = document.querySelector('.profile__name');
 const profileProfession = document.querySelector('.profile__profession');
 const inputName = document.querySelector('.popup__input_type_name');
 const inputProfession = document.querySelector('.popup__input_type_profession');
+// Элементы блока Card
 const cardList = document.querySelector('.elements__list-item');
 const cardTemplate = document.querySelector('#elements__template').content;
+const inputCardName = document.querySelector('.popup__input_type_card-name');
+const inputCardLink = document.querySelector('.popup__input_type_card-link');
 
 const cards = [
   {
@@ -45,25 +50,37 @@ function renderCards() {
   })
 }
 
-function openProfileEdit() {
-  popup.classList.add('popup_opened');
-  inputName.value = profileName.textContent;
-  inputProfession.value = profileProfession.textContent;
+function openPopup(element) {
+  document.querySelector(`.${element}`).classList.add('popup_opened');
+  if(element === 'popup-profile') {
+    inputName.value = profileName.textContent;
+    inputProfession.value = profileProfession.textContent;
+  }
 }
 
 function savePopupEdit(event) {
   event.preventDefault();
   profileName.textContent = inputName.value;
   profileProfession.textContent = inputProfession.value;
-  closePopupEdit();
+  closePopup();
 }
 
-function closePopupEdit() {
-  popup.classList.remove('popup_opened');
+function closePopup(element) {
+  element.classList.remove('popup_opened');
 }
 
 renderCards();
 
-profileEditOpen.addEventListener('click', openProfileEdit);
+popupOpen.forEach(function(element) {
+  element.addEventListener('click', function(event) {
+    return openPopup(event.target.classList.contains('popup-open_profile') ? 'popup-profile' : 'popup-card');
+  })
+})
+
 popupEditSave.addEventListener('click', savePopupEdit);
-popupEditClose.addEventListener('click', closePopupEdit);
+
+popupClose.forEach(function(element) {
+  element.addEventListener('click', function(event) {
+    return closePopup(event.target.closest('.popup'));
+  })
+})
