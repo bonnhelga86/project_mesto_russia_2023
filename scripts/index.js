fb8ef252257dfe97784b83722737f1ab586b18c3
Olga Bondarenko <bonnhelga86@yandex.ru>
2023-02-26 12:46:40 +0400

    feat: Оптимизация index.js и добавление функционала для popup card

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
 const popup = document.querySelector('.popup');
+const popupForm = popup.querySelector('form');
 // Элементы взаимодействия с Popup
 const popupOpen = document.querySelectorAll('.popup-open');
 const popupEditSave = document.querySelector('.popup__button');
@@ -6,13 +7,9 @@ const popupClose = document.querySelectorAll('.popup__close');
 // Элементы блока Profile
 const profileName = document.querySelector('.profile__name');
 const profileProfession = document.querySelector('.profile__profession');
-const inputName = document.querySelector('.popup__input_type_name');
-const inputProfession = document.querySelector('.popup__input_type_profession');
 // Элементы блока Card
 const cardList = document.querySelector('.elements__list-item');
 const cardTemplate = document.querySelector('#elements__template').content;
-const inputCardName = document.querySelector('.popup__input_type_card-name');
-const inputCardLink = document.querySelector('.popup__input_type_card-link');
 
 const cards = [
   {
@@ -41,7 +38,108 @@ const cards = [
   }
 ];
 
+const popupOptions = {
+  profile: () => ({
+    title: 'Редактировать профиль',
+    fields: [
+      {
+        name: 'profile-name',
+        type: 'text',
+        placeholder: 'Ваше имя',
+        class: 'popup__input popup__input_type_name',
+        required: true,
+        value: profileName.textContent
+      },
+      {
+        name: 'profile-profession',
+        type: 'text',
+        placeholder: 'Ваш тип деятельности',
+        class: 'popup__input popup__input_type_profession',
+        required: true,
+        value: profileProfession.textContent
+      }
+    ],
+    buttonText: 'Сохранить',
+    submitHandler: submitProfile,
+  }),
+  card: (values) => ({
+    title: 'Новое место',
+    fields: [
+      {
+        name: 'card-name',
+        type: 'text',
+        placeholder: 'Название',
+        class: 'popup__input popup__input_type_card-name',
+        required: true,
+        value: values['card-name'] || ''
+      },
+      {
+        name: 'card-link',
+        type: 'text',
+        placeholder: 'Ссылка на картинку',
+        class: 'popup__input popup__input_type_card-link',
+        required: true,
+        value: values['card-link'] || ''
+      }
+    ],
+    buttonText: 'Создать',
+    submitHandler: submitCard,
+  })
+}
+
+// Функция динамического заполнения и отображения полей формы для Popup
+function renderPopup(type, values={}) {
+  // Values для предзаполнения карточек exapmple {name: value,...}
+  const { title, fields, buttonText, submitHandler } = popupOptions[type](values) || {};
+  popup.querySelector('.popup__title').textContent = title;
+  // Добавление input в форму Popup
+  fields.forEach(inputAttributes => {
+    const input = document.createElement('input');
+    Object.keys(inputAttributes).forEach( key => input.setAttribute(key, inputAttributes[key]) );
+    popupForm.append(input);
+  })
+  // Добавление кнопки submit в форму Popup
+  const buttonSubmit = document.createElement('button');
+  buttonSubmit.textContent = buttonText;
+  buttonSubmit.classList.add('button', 'popup__button', 'save-popup');
+  popupForm.append(buttonSubmit);
+
+  popupForm.addEventListener('submit', submitHandler);
+
+  openPopup();
+}
+
+// Функция открывания Popup
+function openPopup() {
+  popup.classList.add('popup_opened');
+}
+
+// Функция при Submit Profile
+function submitProfile(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  profileName.textContent = formData.get('profile-name');
+  profileProfession.textContent = formData.get('profile-profession');
+  closePopup();
+}
+
+// Функция при Submit Card
+function submitCard(event) {
+  event.preventDefault();
+  console.log(event);
+
+}
+
+// Функция закрывания Popup
+function closePopup() {
+  popup.classList.remove('popup_opened');
+  popupForm.innerHTML='';
+}
+
+// Функция генерирования карточек
 function renderCards() {
+  // cardList === '' && clearList(cardList.children);
+
   cards.forEach(function(card) {
     const cardItem = cardTemplate.querySelector('.elements__item').cloneNode(true);
     cardItem.querySelector('.elements__photo').src = card.link;
@@ -50,37 +148,18 @@ function renderCards() {
   })
 }
 
-function openPopup(element) {
-  document.querySelector(`.${element}`).classList.add('popup_opened');
-  if(element === 'popup-profile') {
-    inputName.value = profileName.textContent;
-    inputProfession.value = profileProfession.textContent;
-  }
-}
-
-function savePopupEdit(event) {
-  event.preventDefault();
-  profileName.textContent = inputName.value;
-  profileProfession.textContent = inputProfession.value;
-  closePopup();
-}
-
-function closePopup(element) {
-  element.classList.remove('popup_opened');
-}
-
 renderCards();
 
+// Устанавливаются слушатели событий
 popupOpen.forEach(function(element) {
-  element.addEventListener('click', function(event) {
-    return openPopup(event.target.classList.contains('popup-open_profile') ? 'popup-profile' : 'popup-card');
-  })
+  element.addEventListener('click', (event) => renderPopup(event.target.getAttribute('data-popup-type')));
 })
 
-popupEditSave.addEventListener('click', savePopupEdit);
+//Другой вариант вызова функции Popup
+// popupOpen.forEach(function(element) {
+//   element.addEventListener('click', ({ target }) => renderPopup(target.getAttribute('data-popup-type')));
+// })
 
 popupClose.forEach(function(element) {
-  element.addEventListener('click', function(event) {
-    return closePopup(event.target.closest('.popup'));
-  })
+  element.addEventListener('click', (event) => closePopup(event.target.closest('.popup')))
 })
