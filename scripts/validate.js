081f6c26feb78db2aa675a992c6f707c6c3ddc29
Olga Bondarenko <bonnhelga86@yandex.ru>
2023-03-12 08:56:49 +0400

    feat: Подключить скрипт валидации с конфигом

---
+++ scripts/validate.js
@@ -0,0 +1,12 @@
+const configValidation = {
+  formSelector: '.form',
+  inputSelector: '.form__input',
+  submitButtonSelector: '.popup__button',
+  // inactiveButtonClass: 'popup__button_disabled',
+  inputErrorClass: 'form__input_error',
+  errorClass: 'form__text-error_visible'
+}
+
+
+
+enableValidation(configValidation);
